264f41459a2acaef4043d7e2afa21d61bc1a66c7
Erdal256 <erdall.2693@gmail.com>
2021-02-11 00:09:19 +0300

    V6

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //this demek class kendisi demek yani result
+
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+            
+        }
+
+        public Result(bool success) //buna overloding aşırı yükleme denmektedir.
+        {
+            
+            Success = success;
+        }
+
+
+        public bool Success { get; }
+
+        public string Message { get; }
+}
+}
